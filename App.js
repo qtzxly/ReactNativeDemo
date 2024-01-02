cfe8539859177d1338c1262ea8ac6b3e4e3b7e8d
LiuYang <354369716@qq.com>
2024-01-02 17:17:41 +0800

    shadow

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={[styles.box, styles.lightblueBg]}>
+      <View style={[styles.box, styles.lightblueBg, styles.androidShadow]}>
         <Text style={{ borderRadius: 15, backgroundColor: 'pink' }}>
           AAAAAA
         </Text>
       </View>
-      <View style={[styles.box, styles.lightgreenBg]}>
+      <View style={[styles.box, styles.lightgreenBg, styles.boxShadow]}>
         <Text>BBBBBB</Text>
       </View>
     </View>
@@ -16,10 +16,11 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 60, backgroundColor: 'grey' },
+  container: { flex: 1, padding: 60, backgroundColor: 'plum' },
   title: { color: 'red' },
   box: {
-    width: 100,
+    width: 200,
+    height: 200,
     paddingHorizontal: 10,
     paddingVertical: 20,
     marginVertical: 10,
@@ -28,5 +29,15 @@ const styles = StyleSheet.create({
     borderRadius: 15
   },
   lightblueBg: { backgroundColor: 'lightblue' },
-  lightgreenBg: { backgroundColor: 'lightgreen' }
+  lightgreenBg: { backgroundColor: 'lightgreen' },
+  boxShadow: {
+    shadowColor: '#333333',
+    shadowOffset: {
+      width: 6,
+      height: 6
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4
+  },
+  androidShadow: { elevation: 10 }
 })
