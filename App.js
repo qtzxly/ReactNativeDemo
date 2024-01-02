ed72863e0791d192c1cd33c2b94624dd48bfcc07
LiuYang <354369716@qq.com>
2024-01-02 15:25:14 +0800

    styles

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { View, ActivityIndicator, Alert, Button } from 'react-native'
+import {
+  View,
+  ActivityIndicator,
+  Alert,
+  Button,
+  Text,
+  StyleSheet
+} from 'react-native'
 const logImage = require('./assets/adaptive-icon.png')
 import Greet from './components/Greet'
 
 export default function App() {
   return (
-    <View style={{ flex: 1, padding: 60, backgroundColor: 'grey' }}>
+    <View style={styles.container}>
       <Greet name="aaa"></Greet>
       <Greet name="bbb"></Greet>
+      <Text style={styles.title}>style Api</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 60, backgroundColor: 'grey' },
+  title: { color: 'red' }
+})
